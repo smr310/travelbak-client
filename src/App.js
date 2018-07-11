e55cc5f4d0b6a1f80a78aae63c83f0bfbbf6afd3
Steve Romm <smr310@gmail.com>
2018-07-11 09:52:06 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import NavBar from '../components/NavBar';
+import NavBar from './components/NavBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         HELLO WORLD!!!
-        <NavBar />
+      <NavBar />
       </div>
     );
   }
