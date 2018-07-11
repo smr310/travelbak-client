40fdb00d2d844d9e225f2cd699594a8034c9d857
Steve Romm <smr310@gmail.com>
2018-07-11 09:25:20 -0400

    test - wire up first component

---
+++ components/NavBar.js
@@ -0,0 +1,8 @@
+export default function NavBar() {
+    const text = 'Example text';
+    return (
+        <div className="NavBar">
+            {text}
+        </div>
+    );
+};
--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from '../components/NavBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         HELLO WORLD!!!
+        <NavBar />
       </div>
     );
   }
