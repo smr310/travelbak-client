69c98d3fba0babece5b40a3eae291cd82eaf8159
Steve Romm <smr310@gmail.com>
2018-07-11 10:48:55 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import NavBar from './components/navbar';
+import NavBar from './components/NavBar';
 
 class App extends Component {
   render() {
