9166f7dca35b914d052e6eec01824c141658a5e0
Steve Romm <smr310@gmail.com>
2018-07-12 09:06:40 -0400

    test

--- src/components/JournalEntry.js
+++ src/components/JournalEntry.js
@@ -4,14 +4,7 @@ import './JournalEntry.css';
 export default function JournalEntry() {
     return (
         <div className="main-div">
-            <form>
-                <fieldset>
-                    <h4>Date</h4>
-                    <input type="text" className="journal-entry-input" placeholder="" required />
-                    <br />
-                    <button type="submit" className="create-account-btn">Save Journal Entry</button>
-                </fieldset>
-            </form>
+            THIS SHOULD BE THE JOURNAL ENTRY PAGE
         </div>
     );
 };
