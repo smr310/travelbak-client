70465ba9b0be072d2d17309f73a7bfc6d88aa058
Steve Romm <smr310@gmail.com>
2018-07-12 08:36:22 -0400

    test Journal Entries Component

---
+++ src/components/JournalEntries.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './JournalEntries.css';
+
+export default function JournalEntries() {
+    return (
+        <div className="main-div">
+            <div className="journal-entry">Journal Entry One, date, first line...(and Edit + Delete Buttons)</div>
+            <div className="journal-entry">Journal Entry Two, date, first line...(and Edit + Delete Buttons)</div>
+            <div className="journal-entry">Journal Entry Three, date, first line...(and Edit + Delete Buttons)</div>
+            <div className="journal-entry">Journal Entry Four, date, first line...(and Edit + Delete Buttons)</div>
+            <div className="journal-entry">Journal Entry Five, date, first line...(and Edit + Delete Buttons)</div>
+            <div className="journal-entry">Journal Entry Six, date, first line...(and Edit + Delete Buttons)</div>
+        </div>
+    );
+};
--- src/components/Trip.js
+++ src/components/Trip.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Trip.css';
+import JournalEntries from './JournalEntries'
 
 export default function Trip() {
     return (
@@ -10,12 +11,7 @@ export default function Trip() {
                 <h3>Location(s),</h3>
                 <h4>Dates</h4>
                 <div>
-                    <div className="journal-entry">Journal Entry One, date, first line...(and Edit + Delete Buttons)</div>
-                    <div className="journal-entry">Journal Entry Two, date, first line...(and Edit + Delete Buttons)</div>
-                    <div className="journal-entry">Journal Entry Three, date, first line...(and Edit + Delete Buttons)</div>
-                    <div className="journal-entry">Journal Entry Four, date, first line...(and Edit + Delete Buttons)</div>
-                    <div className="journal-entry">Journal Entry Five, date, first line...(and Edit + Delete Buttons)</div>
-                    <div className="journal-entry">Journal Entry Six, date, first line...(and Edit + Delete Buttons)</div>
+                    <JournalEntries />
                 </div>
             </div>   
         </div>
