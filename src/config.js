0b9eaf4f7ddef2faa686f3fbe6246ab5428d1798
Steve Romm <smr310@gmail.com>
2018-08-02 22:17:09 -0400

    test travis

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 export const API_BASE_URL =
-    process.env.REACT_APP_API_BASE_URL || 'https://fathomless-brook-48599.herokuapp.com/api';
+    process.env.REACT_APP_API_BASE_URL || 'https://nameless-bayou-38988.herokuapp.com/api'
